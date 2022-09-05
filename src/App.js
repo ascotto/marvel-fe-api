7508c5831020a2632612e6f657e826299b2c458f
Andrea Scotto Di Minico <ascottodiminico@yahoo.it>
2022-09-06 00:12:48 +0200

    chore: added loading spinner

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import md5 from 'md5'
+import React, { useState, useEffect, useContext } from 'react'
 import { urlWithParams } from './utility/url'
 import Endpoints from './constants/endpoints'
-import { PUBLIC_KEY, PRIVATE_KEY } from './api/api.keys'
-import { Grid } from '@mui/material'
+import { CircularProgress, Grid, Paper } from '@mui/material'
+import TopBarMenu from './components/molecules/TopBarMenu'
+import { Container } from '@mui/system'
+import { GlobalApiParamsState } from './store/params/params.state'
 
 const App = () => {
   const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
   const [open, setOpen] = useState(false)
 
   const defaultResults = 20
 
-  console.log(Endpoints.Comics.Url.Base)
+  const { apikey, offset, ts, hash, format } = useContext(GlobalApiParamsState)
 
   useEffect(() => {
     const fetchData = async () => {
-      const timestamp = Date.now()
-      const publicKey = PUBLIC_KEY
-      const privateKey = PRIVATE_KEY
-
-      const params = {
-        apikey: publicKey,
-        offset: 0,
-        ts: timestamp,
-        hash: md5(timestamp + privateKey + publicKey),
-
-        // format: 'comic',
-      }
-
-      console.log(params)
-
       const apiUrl = Endpoints.Comics.Url.Base
 
-      const fetchURL = urlWithParams(apiUrl, params)
+      setLoading(true)
+
+      const fetchURL = urlWithParams(apiUrl, {
+        apikey,
+        offset,
+        ts,
+        hash,
+        format,
+      })
 
       const results = await fetch(fetchURL)
         .then((res) => res.json())
@@ -41,11 +36,12 @@ const App = () => {
         })
         .then((res) => res.results)
 
+      setLoading(false)
       setData(results)
     }
 
     fetchData()
-  }, [])
+  }, [offset, format])
 
   console.log(data)
 
@@ -62,35 +58,57 @@ const App = () => {
   }
 
   return (
-    <div className="App">
-      <h1>Marvel API</h1>
+    <>
+      <TopBarMenu />
 
-      <Grid container spacing={2}>
-        {data.length > 0 &&
-          data.map((comic, index) => (
-            <Grid item xs={2} key={comic.id.toString()}>
-              <h3>{comic.title}</h3>
-              {index < data.length - 1 ? <hr /> : ''}
+      <div className="App">
+        <Container maxWidth="xl">
+          <Grid
+            container
+            spacing={'18px'}
+            alignItems="stretch"
+            style={{ margin: '0 auto' }}
+          >
+            {loading && (
+              <Grid
+                item
+                xs={12}
+                sx={{ display: 'flex', justifyContent: 'center' }}
+              >
+                <CircularProgress color="inherit" />
+              </Grid>
+            )}
 
-              <p>
-                {comic?.prices.length > 1
-                  ? getLowestPrice(comic.prices)
-                  : comic?.prices[0].price}
-              </p>
+            {data.length > 0 &&
+              !loading &&
+              data.map((comic, index) => (
+                <Grid item xs={6} xl={2} key={comic.id.toString()}>
+                  <Paper sx={{ height: '100%' }}>
+                    <img
+                      alt={comic.title + ' | Marvel Comics'}
+                      src={
+                        comic.thumbnail.path +
+                        '/portrait_fantastic.' +
+                        comic.thumbnail.extension
+                      }
+                    />
 
-              <img
-                alt={comic.title + ' | Marvel Comics'}
-                src={
-                  comic.thumbnail.path +
-                  '/portrait_fantastic.' +
-                  comic.thumbnail.extension
-                }
-              />
-              <button onClick={() => moreInfoHandler(index)}>More info</button>
-            </Grid>
-          ))}
-      </Grid>
-    </div>
+                    <h3>{comic.title}</h3>
+                    <button onClick={() => moreInfoHandler(index)}>
+                      More info
+                    </button>
+                    <p>
+                      {comic?.prices.length > 1
+                        ? getLowestPrice(comic.prices)
+                        : comic?.prices[0].price}
+                    </p>
+                  </Paper>
+                </Grid>
+              ))}
+          </Grid>
+        </Container>
+      </div>
+    </>
   )
 }
 
