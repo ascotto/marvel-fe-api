99c8156fc01aa05071faedc4b3a08dd0facb5da1
Andrea Scotto Di Minico <ascottodiminico@yahoo.it>
2022-09-05 16:48:19 +0200

    code: url utility prettified

--- src/utility/url.js
+++ src/utility/url.js
@@ -1,24 +1,24 @@
 const formatQueryParams = (paramsObject) =>
   Object.keys(paramsObject)
     .filter((key) => {
-      const value = paramsObject[key];
+      const value = paramsObject[key]
       return (
         value !== null &&
         value !== undefined &&
-        value !== "" &&
+        value !== '' &&
         !(Array.isArray(value) && value.length === 0)
-      );
+      )
     })
     .map((key) => {
-      const value = paramsObject[key];
+      const value = paramsObject[key]
       const encodedValue = Array.isArray(value)
-        ? value.map(encodeURIComponent).join(",")
-        : encodeURIComponent(value);
-      return `${key}=${encodedValue}`;
+        ? value.map(encodeURIComponent).join(',')
+        : encodeURIComponent(value)
+      return `${key}=${encodedValue}`
     })
-    .join("&");
+    .join('&')
 
 export const urlWithParams = (url, urlParamsObject) => {
-  const urlParamsQueryString = formatQueryParams(urlParamsObject);
-  return urlParamsQueryString ? `${url}?${urlParamsQueryString}` : url;
-};
+  const urlParamsQueryString = formatQueryParams(urlParamsObject)
+  return urlParamsQueryString ? `${url}?${urlParamsQueryString}` : url
+}
