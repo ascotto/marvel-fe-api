f26a511d0d63a6725b04698eb25cfa4a7e5aa23a
Andrea Scotto Di Minico <ascottodiminico@yahoo.it>
2022-09-07 10:41:22 +0200

    style: added mobile menu styling

--- src/themes/MainTheme.js
+++ src/themes/MainTheme.js
@@ -3,6 +3,10 @@ import { createTheme, ThemeProvider } from '@mui/material/styles'
 import PropTypes from 'prop-types'
 
 const theme = createTheme({
+  body: {
+    overflowX: 'hidden',
+  },
+
   typography: {
     fontFamily: 'Quicksand, sans-serif',
     fontWeight: 400,
@@ -10,6 +14,18 @@ const theme = createTheme({
   },
 
   components: {
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          '&.menu-item': {
+            fontWeight: 'bold',
+            textTransform: 'None',
+            fontSize: '1.125rem',
+          },
+        },
+      },
+    },
+
     MuiButton: {
       styleOverrides: {
         root: {
